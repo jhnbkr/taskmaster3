5e9bc3bb6158c5345ca702acd93bbfb867ac2ebd
John Baker <john@theyoungastronauts.com>
2023-01-24 14:10:23 -0500

    Fixed build error and included missing package

--- components/TaskManager.tsx
+++ components/TaskManager.tsx
@@ -43,7 +43,7 @@ export default function TaskManager() {
         if (!success) error("Something went wrong");
     }
 
-    if (!user) return;
+    if (!user) return <></>;
 
     return (
         <div className="container grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
