e4bf5ee0115ac3b0a00b7efec71e156c347841cf
John Baker <john@theyoungastronauts.com>
2023-01-25 16:04:45 -0500

    Added `meta` description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,10 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
                     rel="stylesheet"
                 />
+                <meta
+                    name="description"
+                    content="Task Master 3 is a simple yet elegant task manager."
+                />
             </Head>
             <body className="bg-slate-100">
                 <Main />
