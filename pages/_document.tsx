d288e5b23f4661e8c93b07f44145f5804215b0bb
John Baker <john@theyoungastronauts.com>
2023-01-25 16:11:22 -0500

    Rearranged `<Head>` tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <meta
+                    name="description"
+                    content="Task Master 3 is a simple yet elegant task manager."
+                />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
                     rel="preconnect"
@@ -13,10 +17,6 @@ export default function Document() {
                 <link
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
                     rel="stylesheet"
-                />
-                <meta
-                    name="description"
-                    content="Task Master 3 is a simple yet elegant task manager."
                 />
             </Head>
             <body className="bg-slate-100">
